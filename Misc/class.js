a0fee29eeb220d419ecce3711233caeb59885847
parshya <prashsingh1997@gmail.com>
2024-06-27 18:06:02 +0530

    Added class

---
+++ Misc/class.js
@@ -0,0 +1,20 @@
+//8. Classes
+//ES6 introduces a more intuitive and structured way to create objects and deal with inheritance through classes.
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+
+    speak() {
+        console.log(`${this.name} makes a noise.`);
+    }
+}
+
+class Dog extends Animal {
+    speak() {
+        console.log(`${this.name} barks.`);
+    }
+}
+
+const dog = new Dog('Rex');
+dog.speak(); // Output: Rex barks.
