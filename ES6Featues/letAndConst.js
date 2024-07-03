4e7498c26a499d6faf29ce3b9ba48dfe3647cf7c
parshya <prashsingh1997@gmail.com>
2024-07-03 20:55:51 +0530

    [Prashant] added comment in letAndConst.js

--- ES6Featues/letAndConst.js
+++ ES6Featues/letAndConst.js
@@ -4,3 +4,20 @@ const b=20;
 a=15;
 //b=25;// this is not allowed
 console.log(`value of a is ${a} and value of b is ${b}`);
+
+
+// Scope:
+// var: Function-scoped.
+// let and const: Block-scoped. within { }
+
+// Hoisting:
+// var: Hoisted to the top of its scope and initialized with undefined.
+// let and const: Hoisted to the top of their block scope but not initialized (temporal dead zone).
+
+// Reassignment:
+// var and let: Can be reassigned.
+// const: Cannot be reassigned after initial assignment.
+
+// Global Object:
+// var: Declares a property on the global object if used in the global scope.
+// let and const: Do not declare properties on the global object if used in the global scope.
