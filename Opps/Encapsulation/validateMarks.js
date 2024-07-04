9563ba9674c0a17f994edf1642ba276e2209b54c
parshya <prashsingh1997@gmail.com>
2024-07-04 18:07:48 +0530

    [Prashant] added oops concepts

---
+++ Opps/Encapsulation/validateMarks.js
@@ -0,0 +1,36 @@
+class Student  
+  {  
+    constructor()  
+    {  
+       var name;  
+       var marks;  
+    }  
+        getName()  
+        {  
+          return this.name;  
+        }  
+      setName(name)  
+      {  
+        this.name=name;  
+      }  
+        
+      getMarks()  
+      {  
+        return this.marks;  
+      }  
+    setMarks(marks)  
+    {  
+        if(marks<0||marks>100)  
+        {  
+          alert("Invalid Marks");  
+        }  
+      else  
+        {  
+          this.marks=marks;  
+        }  
+    }  
+    }  
+    var stud=new Student();  
+     stud.setName("John");  
+     stud.setMarks(110);//alert() invokes  
+     console.log(stud.getName()+" "+stud.getMarks());  
---
+++ Opps/Inheritance/example2.js
@@ -0,0 +1,7 @@
+class Moment extends Date {  
+    constructor(year) {  
+      super(year);  
+    }}  
+  var m=new Moment("August 15, 1947 20:22:10");  
+  console.log("Year value:")  
+  console.log(m.getFullYear());  
